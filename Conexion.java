c53b1a4675825ced8caee198085438c7d0ed1f11
Steffano <noffates2013@gmail.com>
2023-10-15 22:55:21 -0500

    ayuda

---
+++ Conexion.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+public class Conexion {
+  
+    public static Connection obtenerConexion(){
+        String url="jbdc:sqlserver://localhost:1433;"
+                +"database=usuarios;"
+                +"user=sa;"
+                +"password=12345;";
+        
+        try {
+        Connection con = DriverManager.getConnection(url);
+        return con;
+        } catch (SQLException ex){
+            System.out.println(ex.toString());
+            return null;
+        }
+    }
+}
---
+++ Usuario.java
@@ -0,0 +1,86 @@
+public class Usuario {
+
+    private int Id, telf, idtipo;
+    private String tipoU, contra, nombres, apellidos, email, dir, fechaN;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public int getTelf() {
+        return telf;
+    }
+
+    public void setTelf(int telf) {
+        this.telf = telf;
+    }
+
+    public int getIdtipo() {
+        return idtipo;
+    }
+
+    public void setIdtipo(int idtipo) {
+        this.idtipo = idtipo;
+    }
+
+    public String getTipoU() {
+        return tipoU;
+    }
+
+    public void setTipoU(String tipoU) {
+        this.tipoU = tipoU;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDir() {
+        return dir;
+    }
+
+    public void setDir(String dir) {
+        this.dir = dir;
+    }
+
+    public String getFechaN() {
+        return fechaN;
+    }
+
+    public void setFechaN(String fechaN) {
+        this.fechaN = fechaN;
+    }
+    
+}
