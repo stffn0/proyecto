92944e6448e35dc49e0a456c7a619394379db23a
ianthony4 <achaisa@unsa.edu.pe>
2023-10-16 01:07:47 -0500

    Chef - Replanteando

--- src/Chef.java
+++ src/Chef.java
@@ -1,22 +1,32 @@
+import java.util.List;
 
 public class Chef extends Usuario {
-    
-    public void ver_cola_ordenes(){
-        
+    public Chef(int id, String contraseña, String nombre, String apellido, String telefono, String email) {
+        super(id, contraseña, "Chef", nombre, apellido, telefono, email);
     }
-    public void ver_orden(){
-        
+
+    // Método para ver todas las órdenes
+    public void verTodasLasOrdenes(List<Orden> ordenes) {
+        for (Orden orden : ordenes) {
+            System.out.println("Orden ID: " + orden.getId());
+            System.out.println("Número de Mesa: " + orden.getNumeroMesa());
+            System.out.println("Estado: " + orden.getEstado());
+            System.out.println("ID del Mesero: " + orden.getIdMesero());
+            System.out.println();
+        }
     }
-    public void cambiar_estado_orden(){
-        
-    }
-    public void preparar_plato(){
-        
-    }
-    public void nuevo_mesero(){
-        
-    }
-    public void borrar_mesero(){
-        
+
+    // Método para ver una orden específica por su ID
+    public void verOrdenEspecifica(List<Orden> ordenes, int idOrden) {
+        for (Orden orden : ordenes) {
+            if (orden.getId() == idOrden) {
+                System.out.println("Orden ID: " + orden.getId());
+                System.out.println("Número de Mesa: " + orden.getNumeroMesa());
+                System.out.println("Estado: " + orden.getEstado());
+                System.out.println("ID del Mesero: " + orden.getIdMesero());
+                return;
+            }
+        }
+        System.out.println("La orden con ID " + idOrden + " no fue encontrada.");
     }
 }
