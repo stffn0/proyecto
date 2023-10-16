5048d1a575df4eb7b759b39e86e17bc6f23be514
ianthony4 <achaisa@unsa.edu.pe>
2023-10-16 00:36:20 -0500

    Plato - Replanteo

--- src/Plato.java
+++ src/Plato.java
@@ -1,6 +1,34 @@
-public class Plato extends Chef{
-    
-    String idPlato, nombre;
-    double precio;
-    
+public class Plato {
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private String[] ingredientes;
+
+    public Plato(String nombre, String descripcion, double precio, String[] ingredientes) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.ingredientes = ingredientes;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public String[] getIngredientes() {
+        return ingredientes;
+    }
+
+    @Override
+    public String toString() {
+        return "Plato: " + nombre + "\nDescripción: " + descripcion + "\nPrecio: $" + precio + "\nIngredientes: " + String.join(", ", ingredientes);
+    }
 }
