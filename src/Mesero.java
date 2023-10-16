eeefe0f339dbfeb1964b8eb8fd4864f52023d1b4
ianthony4 <achaisa@unsa.edu.pe>
2023-10-16 01:00:22 -0500

    Mesero - replanteo

--- src/Mesero.java
+++ src/Mesero.java
@@ -1,11 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mesero extends Usuario {
-    
-    public void tomar_orden(){
+    private List<Orden> ordenes;
+
+    public Mesero(int id, String contraseña, String tipoDeUsuario, String nombre, String apellido, String telefono, String email) {
+        super(id, contraseña, "Mesero", nombre, apellido, telefono, email);
+        this.ordenes = new ArrayList<>();
     }
-    public void agregar_plato_orden(){     
+
+    // Métodos específicos para Mesero, como tomar orden, agregar plato a la orden, procesar orden
+
+    public void tomarOrden(int idOrden, int numeroMesa) {
+        Orden nuevaOrden = new Orden(idOrden, numeroMesa, getId(), "pendiente");
+        ordenes.add(nuevaOrden);
     }
-    public void procesar_orden(){ 
+
+    public void agregarPlatoAOrden(int idOrden, Plato plato) {
+        for (Orden orden : ordenes) {
+            if (orden.getId() == idOrden && "pendiente".equals(orden.getEstado())) {
+                // Aquí podrías agregar el plato a la orden
+                // Por ejemplo, podrías tener una lista de platos en la clase Orden
+                // y agregar el plato a esa lista.
+                // orden.agregarPlato(plato);
+                return;
+            }
+        }
     }
-    
-   
+
+    public void procesarOrden(int idOrden) {
+        for (Orden orden : ordenes) {
+            if (orden.getId() == idOrden && "pendiente".equals(orden.getEstado())) {
+                orden.setEstado("servida");
+                return;
+            }
+        }
+    }
+
+    public List<Orden> getOrdenes() {
+        return ordenes;
+    }
 }
