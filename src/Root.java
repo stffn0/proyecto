78f3896d0924242ad56d6d3d734b6525ae12dcea
ianthony4 <achaisa@unsa.edu.pe>
2023-10-16 00:32:30 -0500

    Root - Parte 1

--- src/Root.java
+++ src/Root.java
@@ -1,29 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class Root {
+    //Una lista de usuarios
+    private List<Usuario> usuarios;
 
-public class Root extends Usuario{
-    
-    public void crear_usuario(){
-        
+    public Root() {
+        usuarios = new ArrayList<>();
     }
-    public void editar_usuario(){
-        
+
+    // Método para crear un nuevo usuario
+    public void crearUsuario(int id, String contraseña, String tipoDeUsuario, String nombre, String apellido, String telefono, String email) {
+        Usuario nuevoUsuario = new Usuario(id, contraseña, tipoDeUsuario, nombre, apellido, telefono, email);
+        usuarios.add(nuevoUsuario);
     }
-    public void consultar_usuario(){
-        
+
+    // Método para editar un usuario existente
+    public void editarUsuario(int id, String nuevaContraseña, String nuevoTipoDeUsuario, String nuevoNombre, String nuevoApellido, String nuevoTelefono, String nuevoEmail) {
+        for (Usuario usuario : usuarios) {
+            if (usuario.getId() == id) {
+                usuario.setContraseña(nuevaContraseña);
+                usuario.setTipoDeUsuario(nuevoTipoDeUsuario);
+                usuario.setNombre(nuevoNombre);
+                usuario.setApellido(nuevoApellido);
+                usuario.setTelefono(nuevoTelefono);
+                usuario.setEmail(nuevoEmail);
+                break; // Una vez que se encuentra y edita el usuario, se sale del bucle.
+            }
+        }
     }
-    public void eliminar_usuario(){
-        
+
+    // Método para consultar un usuario por ID
+    public Usuario consultarUsuario(int id) {
+        for (Usuario usuario : usuarios) {
+            if (usuario.getId() == id) {
+                return usuario;
+            }
+        }
+        return null; // Usuario no encontrado
     }
-    public void crear_plato(){
-        
-    }
-    public void editar_plato(){
-        
-    }
-    public void consultar_plato(){
-        
-    }
-    public void eliminar_plato(){
-        
+
+    // Método para eliminar un usuario por ID
+    public void eliminarUsuario(int id) {
+        usuarios.removeIf(usuario -> usuario.getId() == id);
     }
 }
