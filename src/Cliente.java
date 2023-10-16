cb54a314a041c3a81ef903307b2e9de9f30e0f50
ianthony4 <achaisa@unsa.edu.pe>
2023-10-16 00:49:13 -0500

    Cliente - Replanteando

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,11 @@
 public class Cliente extends Usuario {
-    
-    
+
+    public Cliente(int id, String contraseña, String nombre, String apellido, String telefono, String email) {
+        super(id, contraseña, "Cliente", nombre, apellido, telefono, email); // Llamamos al constructor de la clase base Usuario
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente ID: " + getId() + "\nNombre: " + getNombre() + " " + getApellido() + "\nTeléfono: " + getTelefono() + "\nEmail: " + getEmail();
+    }
 }
