4e14018096d6b8a7b9af808a0c4c1b778a8aaea2
ianthony4 <achaisa@unsa.edu.pe>
2023-10-16 01:08:17 -0500

    Orden - Replanteando

--- src/Orden.java
+++ src/Orden.java
@@ -1,5 +1,33 @@
-public class Orden extends Plato {
-    
-    String idOrden, idMesero, estado;
-    int mesa;
+public class Orden {
+    private int id;
+    private int numeroMesa;
+    private int idMesero;
+    private String estado;
+
+    public Orden(int id, int numeroMesa, int idMesero, String estado) {
+        this.id = id;
+        this.numeroMesa = numeroMesa;
+        this.idMesero = idMesero;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getNumeroMesa() {
+        return numeroMesa;
+    }
+
+    public int getIdMesero() {
+        return idMesero;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
